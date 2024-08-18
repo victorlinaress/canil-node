86c3d54be84896956fff2f722673584cacb88540
unknown <victorbr8@outlook.com>
2024-08-18 17:17:19 -0300

    Remove node_modules do repositório e atualiza .gitignore

---
+++ src/controllers/pageController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+
+export const home = (req: Request, res:Response) => {
+    res.send('santos');
+
+}
+export const dogs = (req: Request, res:Response) => {
+
+}
+export const cats = (req: Request, res:Response) => {
+
+}
+export const fishes = (req: Request, res:Response) => {
+
+}
+
+export const search = (req: Request, res:Response) => {
+
+}
